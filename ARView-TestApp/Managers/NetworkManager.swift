2adc865b12a1d526902272825e1c80cd044768cc
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-19 22:32:16 +0300

    Update NetworkManager class to handle with loading images from API

--- ARView-TestApp/Managers/NetworkManager.swift
+++ ARView-TestApp/Managers/NetworkManager.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-struct NetworkManager {
+final class NetworkManager {
     
+    let cache         = NSCache<NSString, UIImage>()
     static let shared = NetworkManager()
     private init() {}
     
-    func fetchGames(completed: @escaping (Stream) -> Void) {
-        
+    func fetchGames(completed: @escaping (Result<Stream, CustomError>) -> Void) {
         let stringUrl   = "https://api.twitch.tv/kraken/games/top?limit=100"
         guard let url = URL(string: stringUrl) else { return }
         
@@ -23,18 +23,55 @@ struct NetworkManager {
         request.httpMethod = "GET"
         
         let task = URLSession.shared.dataTask(with: request) { (data, error, _) in
-            guard let data = data else { return }
+            guard let data = data else {
+                completed(.failure(.unableToComplete))
+                return
+            }
             
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let streams = try decoder.decode(Stream.self, from: data)
-                completed(streams)
+                completed(.success(streams))
             } catch {
-                print("Error")
+                completed(.failure(.unableToComplete))
             }
         }
         task.resume()
     }
+    
+    func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey    = NSString(string: urlString)
+        if let image    = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self,
+                  error == nil,
+                  let response = response as? HTTPURLResponse, response.statusCode == 200,
+                  let data = data,
+                  let image = UIImage(data: data) else {
+                    completed(nil)
+                    return
+                }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
+    }
+}
+
+
+enum CustomError: String, Error {
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
 }
 
